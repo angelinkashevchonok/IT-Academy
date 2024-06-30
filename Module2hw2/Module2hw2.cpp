55b9e33a1926e51d429da902ef8cb051064a5263
Angelina Shevchonok <asevcenok68@gmail.com>
2024-06-30 16:40:27 +0300

    added information about compiler

--- Module2hw2/Module2hw2.cpp
+++ Module2hw2/Module2hw2.cpp
@@ -6,17 +6,26 @@
 int main()
 {
 #ifdef _WIN64
-    std::cout << "Your's OS - Windows 64-bit";
+    std::cout << "Your's OS - Windows 64-bit"<<std::endl;
 #elif  _WIN32
-    std::cout << "Your's OS - Windows 32-bit";
+    std::cout << "Your's OS - Windows 32-bit" << std::endl;
 #elif __APPLE__ || __MACH__
-    std::cout << "Your's OS - Mac OSX";
+    std::cout << "Your's OS - Mac OSX" << std::endl;
 #elif __linux__
-    std::cout << "Your's OS -  Linux";
+    std::cout << "Your's OS -  Linux" << std::endl;
 #else
-    std::cout << "Other OS";
+    std::cout << "Other OS" << std::endl;
 #endif 
 
+#ifdef __clang__
+    std::cout << "Compiler - Clang/LLVM" << std::endl;
+#elif __ICC || __INTEL_COMPILER
+    std::cout << "Compiler - Intel ICC/ICPC" << std::endl;
+#elif __GNUC__ || __GNUG__
+    std::cout << "Compiler - GNU GCC/G++" << std::endl;
+#elif _MSC_VER
+    std::cout << "Compiler - Microsoft Visual Studio" << std::endl;
+#endif
 }
 //
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
